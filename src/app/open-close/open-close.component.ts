9ccbfeeab658564d38158790bc5b7f591b738859
Kamahin Mihail <kamahinmihail@gmail.com>
2020-10-27 18:53:10 +0300

    go

--- src/app/open-close/open-close.component.ts
+++ src/app/open-close/open-close.component.ts
@@ -1,29 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-open-close',
   templateUrl: './open-close.component.html',
   styleUrls: ['./open-close.component.scss'],
   animations: [
-    trigger('openClose', [
-      state('open', style({
-        height: '200px',
-        opacity: 1,
-        backgroundColor: 'yellow'
-      })),
-      state('closed', style({
-        height: '100px',
-        opacity: 0.5,
-        backgroundColor: 'green'
-      })),
-      transition('open => closed', [
-        animate('1s')
-      ]),
-      transition('closed => open', [
-        animate('0.5s')
-      ]),
-    ])
+
   ]
 })
 export class OpenCloseComponent implements OnInit {
@@ -31,12 +13,19 @@ export class OpenCloseComponent implements OnInit {
   constructor() { }
 
   isOpen = true;
+  number = 10;
+  items: string[] = ['This is America'];
 
   toggle(): void {
     this.isOpen = !this.isOpen;
   }
 
+  modifyNumber(n: number): void {
+    this.number += n;
+  }
+
   ngOnInit(): void {
+
   }
 
 }
